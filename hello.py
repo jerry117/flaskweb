d5903461a19e4f123cab6d0a08186d92c0fb2628
lijialiang <lijialiang@guorou.tv>
2021-09-22 11:09:27 +0800

    fix:添加说明

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
-from  flask import  Flask
+from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>hello world!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>your browser is %s !</h1>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
@@ -13,4 +15,20 @@ def user(name):
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
+
+# app.url_map  显示URL映射
+
+
+# 上下文的使用例子
+# from flask import current_app
+# app_ctx = app.app_context()
+# app_ctx.push()
+# current_app.name
+# app_ctx.pop()
+
+
+# 支持的4种钩子
+# • before_first_request：注册一个函数，在处理第一个请求之前运行。
+# • before_request：注册一个函数，在每次请求之前运行。
+# • after_request：注册一个函数，如果没有未处理的异常抛出，在每次请求之后运行。
+# • teardown_request：注册一个函数，即使有未处理的异常抛出，也在每次请求之后运行。
