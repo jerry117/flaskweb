b16cccbc2c48c5541de8d470313d294891869e3e
lijialiang <lijialiang@guorou.tv>
2021-09-24 15:22:35 +0800

    表单功能

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ from flask import render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 
@@ -11,13 +14,15 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 # 渲染日期和时间
 moment = Moment(app)
+# 跨站请求伪造保护
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
-def index():
-    user_agent = request.headers.get('User-Agent')
-    # return '<h1>your browser is %s !</h1>' % user_agent
-    # 渲染模板
-    return render_template('index1.html', current_time=datetime.utcnow())
+# @app.route('/')
+# def index():
+#     user_agent = request.headers.get('User-Agent')
+#     # return '<h1>your browser is %s !</h1>' % user_agent
+#     # 渲染模板
+#     return render_template('index1.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
@@ -33,6 +38,20 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+class NameForm(Form):
+    name = StringField('what is your name?', validators=[Required()])
+    submit = SubmitField('submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index1.html', form=form, name=name, current_time=datetime.utcnow())
+
 
 
 
@@ -56,3 +75,36 @@ if __name__ == '__main__':
 # • after_request：注册一个函数，如果没有未处理的异常抛出，在每次请求之后运行。
 # • teardown_request：注册一个函数，即使有未处理的异常抛出，也在每次请求之后运行。
 
+# WTForms支持的HTML标准字段
+# 字段类型        说　明
+# StringField   文本字段
+# TextAreaField  多行文本字段
+# PasswordField  密码文本字段
+# HiddenField    隐藏文本字段
+# DateField      文本字段，值为 datetime.date 格式
+# DateTimeField  文本字段，值为 datetime.datetime 格式
+# IntegerField   文本字段，值为整数
+# DecimalField   文本字段，值为 decimal.Decimal
+# FloatField     文本字段，值为浮点数
+# BooleanField   复选框，值为 True 和 False
+# RadioField     一组单选框
+# SelectField    下拉列表
+# SelectMultipleField 下拉列表，可选择多个值
+# FileField      文件上传字段
+# SubmitField    表单提交按钮
+# FormField      把表单作为字段嵌入另一个表单
+# FieldList      一组指定类型的字段
+
+# WTForms验证函数
+# 验证函数        说　　明
+# Email        验证电子邮件地址
+# EqualTo      比较两个字段的值；常用于要求输入两次密码进行确认的情况
+# IPAddress    验证 IPv4 网络地址
+# Length       验证输入字符串的长度
+# NumberRange  验证输入的值在数字范围内
+# Optional     无输入值时跳过其他验证函数
+# Required     确保字段中有数据
+# Regexp       使用正则表达式验证输入值
+# URL          验证 URL
+# AnyOf        确保输入值在可选值列表中
+# NoneOf       确保输入值不在可选值列表中
