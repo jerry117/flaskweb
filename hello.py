9a2ca2c8f26e1ef16c7c0ef92abdb440fc189ea5
lijialiang <lijialiang@guorou.tv>
2021-10-08 15:52:13 +0800

    视图中操作数据库

--- hello.py
+++ hello.py
@@ -99,15 +99,30 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # form = NameForm()
+    # if form.validate_on_submit():
+    #     old_name = session.get('name')
+    #     if old_name is not None and old_name != form.name.data:
+    #         flash('Looks like you have changed your name!')
+    #     session['name'] = form.name.data
+    #     # 重定向到这个地址
+    #     return redirect(url_for('index'))
+    # return render_template('index1.html', form = form, name = session.get('name'), current_time=datetime.utcnow())
+
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         # 重定向到这个地址
         return redirect(url_for('index'))
-    return render_template('index1.html', form = form, name = session.get('name'), current_time=datetime.utcnow())
+    return render_template('index1.html', form = form, name = session.get('name'), known = session.get('known', False), current_time=datetime.utcnow())
 
 
 if __name__ == '__main__':
