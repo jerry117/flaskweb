d3dcbd2dbf60458024c6011e88385b73ee83a747
lijialiang <lijialiang@guorou.tv>
2021-12-22 11:55:57 +0800

    调整样式

--- uni-app/src/pages/users/index.vue
+++ uni-app/src/pages/users/index.vue
@@ -1,24 +1,45 @@
 <template>
-	<view class="content">
-		<view>
+    <view class="login-wrapper">
+        
+		<!-- <view>
 			<text class="title">{{title}}</text>
-		</view>
-        <view class="phone-mask">
-            <view class="input-item">
-            
-            <input type="text" v-model="account" placeholder="输入账号" maxlength='11' >
-            </view>
-            <view class="input-item">
-                
-            <input type="text" v-model="password" placeholder="输入密码" maxlength='11' >
-            </view>
-            <view class="sub_btn" @click="loginBtn">立即登录</view>
+		</view> -->
+        <view class="shading">
+            <image class='logo' src="/static/logo.png" ></image>
         </view>
-	</view>
+
+        <view class="whiteBg">
+            <view class="list">
+                <form @submit.prevent="submit">
+                    <view class="item">
+                        <view class="acea-row">
+                            <image src="/static/images/phone_1.png" style="width: 24rpx; height: 34rpx;"></image>
+                            <input class='input' type="text" v-model="account" placeholder="输入手机号码" maxlength='11' required >
+                        </view>
+                    </view>
+                    <view class="item">
+                        <view class="acea-row">
+                            <image src="/static/images/code_1.png" style="width: 28rpx; height: 32rpx;"></image>
+                            <input class='input' type="password" v-model="password" placeholder="填写登录密码" required >
+                        </view>
+                    </view>
+                </form>
+
+            </view>
+
+
+            <view class="logon" @click="submit">登录</view>
+        </view>
+
+    </view>
+
+
 </template>
 
 <script>
+// import image from '../API/image/image.vue'
 	export default {
+//   components: { image },
 		data() {
 			return {
 				title: '登录',
@@ -30,32 +51,80 @@
 
 		},
 		methods: {
+            submit(){
+                console.log(111)
+            }
 
 		}
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+<style >
+    
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin: 200rpx auto 50rpx auto;
-	}
+.login-wrapper {
+    padding: 30rpx;
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+    
+    
+}
+.shading {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        margin-top: 50rpx;
+        /* margin-top: 200rpx; */
+        
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+    }
+
+.logo {
+    height: 200rpx;
+    width: 200rpx;
+    margin: 200rpx auto 50rpx auto;
+}
+.whiteBg {
+        margin-top: 100rpx;
+        
+    }
+
+.list {
+            border-radius: 16rpx;
+            overflow: hidden;
+            
+        }
+
+.item {
+            border-bottom: 1px solid #f0f0f0;
+            background: #fff;
+        }
+
+.acea-row {
+    margin-left: 80rpx;
+    display: inline-flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+    
+}
+
+.logon {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 86rpx;
+    margin-top: 80rpx;
+    background-color: red;
+    border-radius: 120rpx;
+    color: white;
+    font-size: 30rpx;
+}
+
+.input {
+    margin-left: 40rpx;
+}
+
+    
 </style>
