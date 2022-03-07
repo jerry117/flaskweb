097b915f82f0d04b3d1d7b72635b09e67ea18d4f
lijialiang <614237720@qq.com>
2022-03-07 21:51:35 +0800

    fix

--- market.py
+++ market.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template()
+    return render_template('hello.html')
 
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
